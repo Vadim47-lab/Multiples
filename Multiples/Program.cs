d86513b445e46b8c9afe7f478ef5083276bf9db9
Stepanov Vadim <vadgat777@mail.ru>
2022-07-01 16:46:23 +0700

    first commit

---
+++ Multiples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Multiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Программа кратные числа:");
+
+            Random random = new Random();
+            int sum = 0;
+            int nextNumber = 1;
+            int m = 1000;
+
+            int N = random.Next(1, 27);
+
+            for (int i = 0; i < m; i = i + nextNumber + N)
+            {
+                sum++;
+            }
+
+            Console.WriteLine("\n Количество трехзначных натуральных чисел sum = " + sum + ", которые кратны N = " + N + ".");
+        }
+    }
+}
